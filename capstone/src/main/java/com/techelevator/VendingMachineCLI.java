fc7e74bf02d55856b223c7ca0f4f2f6c4a5e5d27
mike levy <michael.levy@techelevator.com>
2022-10-04 12:00:19 -0400

    init repo

---
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class VendingMachineCLI {
+
+
+	public VendingMachineCLI() {
+	}
+
+	public void run() {
+	}
+
+	public static void main(String[] args) {
+		VendingMachineCLI cli = new VendingMachineCLI();
+		cli.run();
+	}
+}
